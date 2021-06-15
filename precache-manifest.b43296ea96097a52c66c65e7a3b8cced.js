4c954d2668719e7265ff5c034465dac2325f78ae
1997roylee <53046899+1997roylee@users.noreply.github.com>
2021-06-16 03:01:42 +0800

    Updates

---
+++ precache-manifest.b43296ea96097a52c66c65e7a3b8cced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e38c0c23a5b4b747e69c3be0fc47b33",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84cd04e153dcaaeaa507",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "74fa0ef06d50ee4f5db9",
+    "url": "./static/js/2.cdb44cad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cdb44cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cd04e153dcaaeaa507",
+    "url": "./static/js/main.aaaa75ee.chunk.js"
+  },
+  {
+    "revision": "d5ac5980603689c62333",
+    "url": "./static/js/runtime-main.152a398d.js"
+  }
+]);
